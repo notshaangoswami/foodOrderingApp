b6c3a262418a189077f80809a725d32a7cb1e52b
notshaangoswami <shaangoswami18@gmail.com>
2023-09-01 12:20:24 +0530

    eps-03

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//component composition
+//-component inside a component
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "xyz" },
-  "Hello World from React"
-); //object
-console.log(heading);
+const title = (
+  <h1 id="heading" tabIndex="5">
+    Namaste Shaan Using jsx
+  </h1>
+);
+
+const HeadingComponent = () => (
+  <div id="container">
+    {title}  //js using '{}''
+    Namaste Shaan Functional Component
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeadingComponent />); //functional Component
