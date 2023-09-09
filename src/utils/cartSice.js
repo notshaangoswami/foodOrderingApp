a8c75f2733102ac8ef451933f52fb701027c1ca2
notshaangoswami <shaangoswami18@gmail.com>
2023-09-09 11:56:46 +0530

    eps-12

--- src/utils/cartSice.js
+++ src/utils/cartSice.js
@@ -7,13 +7,28 @@ const cartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+      //vanilla(older Redux)=>DONT HAVE TO MUTATE THE STATE
+      //const newState=[...state](make new variable);
+      //newState.push(action.payload);
+      //return newState;
+
+      //Redux Tool kit-->uses "immer" behind the scenes 
+      //WE HAVE TO MUTATE THE STATE
       state.items.push(action.payload);
     },
     removeItem: (state) => {
       state.items.pop();
     },
     clearCart: (state) => {
-      state.items.length = 0;
+
+      //state=[]-->wont work because we are not mutating the state
+
+      //RTK-->EITHER MUTATE THE STATE OR RETURN STATE
+      //1.Muatate the state
+       state.items.length = 0;
+
+       //2.Return
+       //return {items:[]};
     },
   },
 });
